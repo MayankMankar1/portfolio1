b722d1683a0c8f183c61ffa3de2dfc8d32c53e5e
MayankMankar1 <140330421+MayankMankar1@users.noreply.github.com>
2025-08-28 00:03:01 +0530

    zz

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//    base: command === "build" ? "/portfolio1/" : "/",
+//   // base:"/portfolio1/",
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base:"/portfolio1/",
-});
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react(),tailwindcss()],
+    // base: command === "build" ? "/portfolio1/" : "/",
+    base:"/portfolio/"
+  }
+})
+
